eaa6713dc6a394e2467dfcb7f901272e82c10e44
Bartosz <bart.kaczor@gmail.com>
2024-09-23 11:13:29 +0200

    Add empty Log component, refactor board state

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import { useState } from "react"
 import GameBoard from "./components/GameBoard"
 import Player from "./components/Player"
+import Log from "./components/Log"
 
 function App() {
+  const [gameTurns, setGameTurns] = useState([])
   const [activeSymbol, setactiveSymbol] = useState("X")
 
-  function handleSelectSquare() {
+  function handleSelectSquare(rowIndex, colIndex) {
     setactiveSymbol(activeSymbol === "X" ? "O" : "X")
+
+    setGameTurns(prevTurns => {
+      let currentSymbol = activeSymbol
+
+      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
+        currentSymbol = "O"
+      }
+
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, symbol: currentSymbol },
+        ...prevTurns
+      ]
+
+      return updatedTurns
+    })
   }
 
   return <main>
@@ -15,9 +32,11 @@ function App() {
         <Player active={activeSymbol === "X"} initialName="Player 1" symbol="X" />
         <Player active={activeSymbol === "O"} initialName="Player 2" symbol="O" />
       </ol>
-      <GameBoard onSelectSquare={handleSelectSquare} activeSymbol={activeSymbol} />
+      <GameBoard
+        onSelectSquare={handleSelectSquare}
+        turns={gameTurns} />
     </div>
-    LOG
+    <Log />
   </main>
 }
 
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -6,27 +6,22 @@ const initialGameBoard = [
   [null, null, null]
 ]
 
-export default function GameBoard({ onSelectSquare, activeSymbol }) {
-  const [board, setBoard] = useState(initialGameBoard)
+export default function GameBoard({ onSelectSquare, turns }) {
+  let gameBoard = initialGameBoard
 
-  function handleSymbolChange(row, col, value) {
-    setBoard(prevBoard => {
-      const newBoard = [...prevBoard.map(row => [...row])]
-      newBoard[row][col] = value
-      return newBoard
-    })
-
-    onSelectSquare()
+  for(const turn of turns) {
+    const { square, symbol } = turn
+    gameBoard[square.row][square.col] = symbol
   }
 
   return(
     <ol id="game-board">
-      {board.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((symbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => handleSymbolChange(rowIndex, colIndex, activeSymbol)}>{symbol}</button>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{symbol}</button>
               </li>
             ))}
           </ol>
